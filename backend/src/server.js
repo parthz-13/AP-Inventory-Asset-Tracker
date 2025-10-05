9ad8733b005a2804717f1fcd43c7dbc84ea464f6
p4rth <parthsingh1006@gmail.com>
2025-10-05 16:57:40 +0530

    server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,4 +13,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = 5000
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
