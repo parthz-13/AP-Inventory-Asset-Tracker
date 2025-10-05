f71764200edd11db3ccea541d18a3952c16cc00b
p4rth <parthsingh1006@gmail.com>
2025-10-05 16:05:36 +0530

    backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { PrismaClient } from "../../generated/prisma/client.js";
+
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Inventory & Asset Tracker Backend is Running");
+});
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
