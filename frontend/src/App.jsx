ea1fda7d0c596bc7a2858469a35e1c93b10ee059
Veer Shah <veershah@Veers-MacBook-Pro.local>
2025-11-03 23:34:17 +0530

    demo added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/home.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Home />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
