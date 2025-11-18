c5a6834de1188fb6a3fef3e849ead4045a0b2bd5
Veer Shah <veershah@Veers-MacBook-Pro.local>
2025-11-18 11:31:51 +0530

    item creation,deletion,findings and modification added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { prisma } from "./config/prisma.js";
 import authRoutes from "./routes/auth.js";
+import itemRoutes from "./routes/items.js";
+
 
 dotenv.config();
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+
+app.use("/items", itemRoutes);
 
 app.get("/", (req, res) => {
   res.send("Inventory & Asset Tracker Backend is Running");
